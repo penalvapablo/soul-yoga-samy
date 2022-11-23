0759c43a7759c5fa3c58fe85f8ce85cc274dcec0
Pablo Penalva <penalva.pablo@gmail.com>
2022-11-23 16:15:27 -0300

    try fix analytics 2

--- pages/_app.js
+++ pages/_app.js
@@ -6,15 +6,16 @@ config.autoAddCss = false
 // 
 
 function MyApp({ Component, pageProps }) {
+  console.log('hola')
   return (<>
-    <Script strategy='lazyOnload' src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXNEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+    <Script strategy='lazyOnload' src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
     <Script strategy='lazyOnload' id='google-tag'>
       {
         `window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
 
-      gtag('config', '${process.env.NEXNEXT_PUBLIC_GOOGLE_ANALYTICS}');`
+      gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');`
       }
     </Script>
     <Component {...pageProps} />
