043ba9c5833be586bdd84f89ce8cd35f41acd6f1
Pablo Penalva <penalva.pablo@gmail.com>
2022-11-23 15:51:01 -0300

    google-analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,24 @@
 import '../styles/globals.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import Script from 'next/script'
 config.autoAddCss = false
 // 
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (<>
+    <Script strategy='lazyOnload' src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXNEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+    <Script strategy='lazyOnload' id='google-tag'>
+      {
+        `window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', ${process.env.NEXNEXT_PUBLIC_GOOGLE_ANALYTICS});`
+      }
+    </Script>
+    <Component {...pageProps} />
+  </>)
 }
 
 export default MyApp
